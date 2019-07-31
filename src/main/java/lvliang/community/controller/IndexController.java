85fd605629862b0e16e1d40dd36b6f13fe7bea69
lv-liang <15228638110@163.com>
2019-07-31 11:09:50 +0800

    add index

---
+++ src/main/java/lvliang/community/controller/IndexController.java
@@ -0,0 +1,16 @@
+package lvliang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    /*
+     * "/"为根目录
+     */
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
